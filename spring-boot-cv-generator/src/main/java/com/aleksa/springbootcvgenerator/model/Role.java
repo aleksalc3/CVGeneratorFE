8f88ff13c1201af2c81b2e22440e8ec1a6362302
Aleksa Ilic <aleksalc3@gmail.com>
2022-07-03 17:42:24 +0200

    not

--- spring-boot-cv-generator/src/main/java/com/aleksa/springbootcvgenerator/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.aleksa.springbootcvgenerator.model;
-
-public enum Role {
-    USER,//default
-    ADMIN,//crud
-    SYSTEM_MANAGER//internal operations
-}
--- spring-boot-cv-generator/src/main/java/com/aleksa/springbootcvgenerator/service/IAuthenticationService.java
+++
@@ -1,7 +0,0 @@
-package com.aleksa.springbootcvgenerator.service;
-
-import com.aleksa.springbootcvgenerator.model.User;
-
-public interface IAuthenticationService {
-    User signInAndReturnJWT(User signInRequest);
-}
